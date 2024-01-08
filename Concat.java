d2af06f2c66c9de35467aef45038b970425ca254
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-01-08 21:12:03 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,10 @@
+public class Concat{
+public static void main(String[]args){
+// string concatenation
+System.out.println("hello" + "world"); 
+
+String var1 = "hello", var2 = "world";
+int num = 5;
+System.out.println(var1 + var2 + num);
+}
+}

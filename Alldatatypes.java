b6cd13cc2c0fefeba885b2414dc8ad76e25912bf
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-18 23:44:41 +0530

    Add files via upload

---
+++ Alldatatypes.java
@@ -0,0 +1,21 @@
+public class Alldatatypes{
+public static void main(String[]args){
+byte byteVar = 127;
+short shortVar =32767;
+int intVar = 2147483647;
+long longVar = 9223372036854775807L;
+double doubleVar = 3.14159265359;
+float floatVar = 3.14f; 
+char charVar = 'A';
+boolean booleanVar = true; 
+
+System.out.println("Byte Variable: " + byteVar);
+System.out.println("Short Variable: " + shortVar);
+System.out.println("int Variable: " + intVar);
+System.out.println("Long Varible: "+ longVar);
+System.out.println("Double Variable: " + doubleVar);
+System.out.println("float Variable: " + floatVar);
+System.out.println("Char Variable: " + charVar);
+System.out.println("Boolean Variable: " + booleanVar);
+}
+}

ed84d6ba76dae734e65c2fae29f970b55c9236a4
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-18 23:52:00 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial{
+
+public static void main(String[]args){
+int num = 4;
+int Fact = 1;
+for (int i = 1; i<=num; i++){
+Fact = Fact * i;
+
+}
+System.out.println(Fact);
+}
+}

3139fe8945fb8fb1ec90c2ade409f0a3768d271b
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-18 23:42:55 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A{
+
+public  static void main (String[]args){
+double num1 = 5.5;
+double num2 = 10.5;
+double num3 = 15.5;
+double Average = (num1+num2+num3)/2.0;
+System.out.println(Average);
+}
+}

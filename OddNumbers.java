49cbdc1b9c4f78b3ab39d6975b6c6f4fe454a25c
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-19 00:01:34 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,12 @@
+public class OddNumbers{
+public static void main(String[]args){
+
+int a =0;
+
+for(int i=51; i<=99; i+=2){
+a+=i;
+}
+System.out.println("Sum of the Odd Numbers between 50 to 100:"+a);
+
+}
+}

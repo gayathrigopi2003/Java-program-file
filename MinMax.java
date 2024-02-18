99f096623419b9b5e2c441dfa29eeeb43ad75603
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-18 23:55:19 +0530

    Add files via upload

---
+++ MinMax.java
@@ -0,0 +1,14 @@
+public class MinMax{
+
+public static void main(String[]args){
+int a[] = {0,1,2,3,4,5,6,};
+int min = a[0];
+
+for (int i = 1; i<a.length; i++){
+if(a[i]<min){
+min = a[i];
+}
+}
+System.out.print(a[i]);
+}
+}

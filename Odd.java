a3d172941f90e4aa0c9345754a9869eb478c9edf
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-19 00:01:08 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,14 @@
+public class Odd{
+public static void main(String[]args){
+int a = 50;
+int b= 100;
+
+System.out.println("odd Numbers:");
+for(int i = a; i<=b; i++){
+  if(i%2!=0){
+  System.out.println(i+" ");
+  }
+
+}
+}
+}

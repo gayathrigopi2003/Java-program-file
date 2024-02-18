984d9e2529f0f56802874f0e837dc8708de76c25
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-18 23:51:19 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,24 @@
+public class Even{
+
+
+public static void main(String[]args){
+	
+	Even M = new Even();
+	System.out.println(M.LargestOddNumber("98385"));
+}
+
+
+public void LargestOddNumber(String args){
+	int a;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    								 
+	
+for(int i=a.length()-1; i>=0;i--){
+	char c =a.charAt(i);
+
+if(c%2==1){
+    return a.subString(0,i+1);
+}
+
+} 
+return"";
+}
+}

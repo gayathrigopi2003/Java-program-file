dea42504ce17891ac331deff8bef6eb444f42913
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-19 00:07:55 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class UserInput{
+public static void main(String[]args){
+	
+Scanner scan = new Scanner(System.in);
+String input;
+
+System.out.println("Enter a string :");
+input = scan.nextLine();
+
+System.out.println("you Enter the string value: "+input);
+
+
+}
+}

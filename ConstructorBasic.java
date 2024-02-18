3673d60a2b9aacc8994ba9a5ca6d7528228f2848
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-18 23:48:55 +0530

    Add files via upload

---
+++ ConstructorBasic.java
@@ -0,0 +1,13 @@
+public class ConstructorBasic{
+
+ConstructorBasic(int n){
+ System.out.println(n);
+}
+public static void main (String[]args){
+
+ConstructorBasic m = new ConstructorBasic(5);
+
+}
+
+
+}

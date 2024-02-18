eb965f635342193d861c987035fb7f97c069758d
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-19 00:07:39 +0530

    Add files via upload

---
+++ Tables.java
@@ -0,0 +1,22 @@
+public class Tables{
+public static void main(String[]args){
+	
+for (int n=1; n<=10; n++){
+System.out.println("3*"+n+"="+3*n);
+}
+
+int n = 1;
+while(n<=10){
+	System.out.println("4* "+n+"="+4*n);
+	n++;
+}
+
+int j = 1;
+do{
+ System.out.println("5* "+j+"="+5*j);
+ j++;
+}while(j<=10);
+
+
+}
+}

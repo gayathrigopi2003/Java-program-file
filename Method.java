241143343f6c97a2fa19c138f5d9185e68aa9ebd
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-18 23:55:05 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,16 @@
+public class Method{
+
+void printHello(String str){
+
+System.out.println(str);
+
+}
+
+public static void main(String[]args){
+
+Method m = new Method();
+m.printHello("Gayu");
+
+}
+
+}

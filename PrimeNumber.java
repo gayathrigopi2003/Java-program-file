c2787341ea537cbeaa80431ff73f51965bb003ed
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-19 00:02:47 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,25 @@
+public class PrimeNumber{
+
+public static void main (String[]args){
+
+int num = 4;
+int count = 0;
+
+for(int i=1; i<=num; i++){
+
+if(num% i == 0){
+count++;
+}
+}
+if(count == 2){
+System.out.println("It is a prime number");
+
+}
+else {
+System.out.println("It is not a prime number");
+}
+
+
+
+}
+}

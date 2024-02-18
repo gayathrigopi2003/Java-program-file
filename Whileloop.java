e9f3baf4e1d8ad8a1afc26f9ab4ab1f2c50617ac
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-19 00:08:36 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,17 @@
+public class Whileloop{
+
+public static void main (String[]args){
+int i = 0;
+while(i<=45){
+System.out.print(i);
+i++; 
+}
+System.out.println();
+
+int j = 45;
+do{
+System.out.print(j);
+j++;
+}while(j<=55);
+}
+}

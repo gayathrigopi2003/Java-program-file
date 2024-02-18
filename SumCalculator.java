8f248c3b6b59c499e569a45b1aadff66820477d0
gayathrigopi2003 <128364793+gayathrigopi2003@users.noreply.github.com>
2024-02-19 00:05:41 +0530

    Add files via upload

---
+++ SumCalculator.java
@@ -0,0 +1,24 @@
+public class SumCalculator {
+    public static void main(String[] args) {
+        
+        if (args.length != 2) {
+            System.out.println("Please provide exactly two numbers as command-line arguments.");
+            return;
+        }
+
+
+        try {
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
+
+        
+            int sum = num1 + num2;
+
+         
+            System.out.println("Sum: " + sum);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please provide valid integers as command-line arguments.");
+        }
+    }
+}
